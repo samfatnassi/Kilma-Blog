333985af782131490ea8e8bcea42e12f515369b7
Houssem <houcemfatnassi@gmail.com>
2026-09-24 09:43:07 +0100

    chore: rebrand blog to Kilma Labs with new tagline

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Kilma AI';
+    : 'Kilma Labs';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'AI Voice, Human Touch';
+    : 'Building Tomorrow’s Intelligence';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
